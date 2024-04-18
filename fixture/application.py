a84baeb322cc31218cf59038961d57df3726b490
peachlex <peachlexx@gmail.com>
2024-04-18 20:12:41 +0300

    Add conftest file Add deletion test

--- fixture/application.py
+++ fixture/application.py
@@ -13,12 +13,9 @@ class Application:
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
-
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/index.php")
-
-
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
+
 from model.group import Group
-from fixture.application import Application
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_group(app):
@@ -16,10 +8,8 @@ def test_add_group(app):
     app.group.create(Group("test", "test", "test"))
     app.session.logout()
 
+
 def test_add_empty_group(app):
     app.session.login("admin", "secret")
     app.group.create(Group("", "", ""))
     app.session.logout()
-
-
-
