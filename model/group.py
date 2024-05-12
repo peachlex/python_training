f0257075461f375e7611335640f8e16b05690fd0
peachlex <peachlexx@gmail.com>
2024-05-12 20:34:14 +0300

    Add get list of groups

--- model/group.py
+++ model/group.py
@@ -1,6 +1,7 @@
 class Group:
 
-    def __init__(self, name, header, footer):
+    def __init__(self, name=None, header=None, footer=None, group_id=None):
         self.name = name
         self.header = header
         self.footer = footer
+        self.id = group_id
--- test/test_edit_first_group.py
+++ test/test_edit_first_group.py
@@ -4,10 +4,16 @@ from model.group import Group
 def test_edit_name_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group("test", None, None))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group('new', None, None))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 
 def test_edit_header_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group("test", None, None))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(None, "new", None))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
