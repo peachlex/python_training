e596255826787c5a3d893943b88487a7e807f1cb
peachlex <peachlexx@gmail.com>
2024-06-02 18:58:47 +0300

    add contacts lists comparison in edit test and delete teste

--- test/test_edit_first_contact.py
+++ test/test_edit_first_contact.py
@@ -1,13 +1,17 @@
 from model.contact import Contact
 
 
-def test_edit_fname_first_contact(app):
+def test_edit_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact("test", None, None))
-    app.contact.edit_first_contact(Contact('Modified', None, None ))
+    old_contacts = app.contact.get_contacts_list()
+    contact = Contact('Modified', 'Modified', 'Modified')
+    contact.contact_id = old_contacts[0].contact_id
+    app.contact.edit_first_contact(contact)
+    new_contacts = app.contact.get_contacts_list()
+    assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-def test_edit_lname_first_contact(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact("test", None, None))
-    app.contact.edit_first_contact(Contact(None, None, 'Modified' ))
+
