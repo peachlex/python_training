1e89fe8155f52916ddb8ed124aa3386a68103e35
peachlex <peachlexx@gmail.com>
2024-06-09 21:15:26 +0300

    add parser

--- test/conftest.py
+++ test/conftest.py
@@ -7,14 +7,16 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
+    usr_pass = request.config.getoption("--usrPass")
+    user = request.config.getoption("--user")
     if fixture is None:
-        fixture = Application()
-        fixture.session.login(username="admin", password="secret")
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
-            fixture.session.login(username="admin", password="secret")
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url)
+    fixture.session.ensure_login(username=user, password=usr_pass)
     return fixture
 
 
@@ -26,3 +28,11 @@ def stop(request):
 
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/index.php")
+    parser.addoption("--usrPass", action="store", default="secret")
+    parser.addoption("--user", action="store", default="admin")
+
