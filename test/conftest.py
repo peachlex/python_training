d1bf4d7a59ff859a28302b41a0c8e84dac2504c3
peachlex <peachlexx@gmail.com>
2024-06-11 22:13:31 +0300

    add path autodetection

--- test/conftest.py
+++ test/conftest.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 
 import pytest
 from fixture.application import Application
@@ -12,8 +13,9 @@ def app(request):
     global target
     browser = request.config.getoption("--browser")
     if target is None:
-        with open(request.config.getoption("--target")) as config_file:
-            target = json.load(config_file)
+        config_file = os.path.abspath(f'../{request.config.getoption("--target")}')
+        with open(config_file) as f:
+            target = json.load(f)
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=target['baseUrl'])
     fixture.session.ensure_login(username=target['user'], password=target['usrPass'])
