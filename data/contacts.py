dca202552c442b93a5d7fe8b15dddd8f427ea02c
peachlex <peachlexx@gmail.com>
2024-06-12 13:11:16 +0300

    add data/contacts.py

---
+++ data/contacts.py
@@ -0,0 +1,7 @@
+from model.contact import Contact
+
+testdata = [Contact(first_name="FirstName", last_name="LastName",
+                    homephone="8192893819", mobilephone="1937491347",
+                    workphone="8918764518", address="CityName",
+                    email1="somemail1", email2="somemail2", email3="somemail3"
+)]
