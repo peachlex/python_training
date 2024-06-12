accea6c5193965ab9612205a389c29cea3b6db5b
peachlex <peachlexx@gmail.com>
2024-06-12 15:11:47 +0300

    add read groups from db and test for matching ui and db

--- test/conftest.py
+++ test/conftest.py
@@ -2,6 +2,7 @@ import importlib
 import json
 import os.path
 import jsonpickle
+from fixture.db import DbFixture
 
 import pytest
 from fixture.application import Application
@@ -10,18 +11,22 @@ fixture = None
 target = None
 
 
+def load_config(file):
+    global target
+    if target is None:
+        config_file = os.path.abspath(f'../{file}')
+        with open(config_file) as f:
+            target = json.load(f)
+    return target
+
 @pytest.fixture
 def app(request):
     global fixture
-    global target
     browser = request.config.getoption("--browser")
-    if target is None:
-        config_file = os.path.abspath(f'../{request.config.getoption("--target")}')
-        with open(config_file) as f:
-            target = json.load(f)
+    web_config = load_config(request.config.getoption("--target"))['web']
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=target['baseUrl'])
-    fixture.session.ensure_login(username=target['user'], password=target['usrPass'])
+        fixture = Application(browser=browser, base_url=web_config['baseUrl'])
+    fixture.session.ensure_login(username=web_config['user'], password=web_config['usrPass'])
     return fixture
 
 
@@ -58,3 +63,12 @@ def load_from_module(module):
 def load_from_json(file):
     with open(os.path.join(os.path.abspath(os.path.abspath(f"../data/{file}.json")))) as f:
         return jsonpickle.decode(f.read())
+
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))['db']
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+    def fin():
+        dbfixture.destroy
+    request.addfinalizer(fin)
+    return dbfixture
